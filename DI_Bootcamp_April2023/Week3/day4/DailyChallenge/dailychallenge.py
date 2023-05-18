2f50bb5f87ba2f97500c66a48b43c238c5f89973
Merylad <meryl.adjedj@gmail.com>
2023-05-18 10:52:09 +0300

    rectify DL

--- DI_Bootcamp_April2023/Week3/day4/DailyChallenge/dailychallenge.py
+++ DI_Bootcamp_April2023/Week3/day4/DailyChallenge/dailychallenge.py
@@ -4,7 +4,7 @@ sentence = 'A good book would sometimes cost as much as a good house.'
 
 class Text :
     def __init__(self, text) :
-        self.text = text.lower().replace('.','').replace(',','').replace("'",' ').replace('!','').replace('?','').replace('\n','').split(' ')
+        self.text = text.lower().replace('.','').replace(',','').replace("'",' ').replace('!','').replace('?','').replace('\n','')
         
 
 #a method to return the frequency of a word in the text (assume words are separated by whitespace) return None or a meaningful message.
@@ -12,7 +12,7 @@ class Text :
     def frequency_word (self, word) :
         num = 0
         word = word.lower()
-        for current_word in self.text :
+        for current_word in self.text.split(' ') :
             if current_word == word :
                 num +=1
         return num
@@ -20,11 +20,11 @@ class Text :
     def common_word (self) :
         num = 0
         most_common = []
-        for word in self.text :
+        for word in self.text.split(' ') :
             if self.frequency_word(word) > num:
                 num = self.frequency_word(word)
 
-        for word in self.text :
+        for word in self.text.split(' ') :
             if self.frequency_word(word) == num :
                 if word not in most_common:
                     most_common.append(word)
@@ -34,7 +34,7 @@ class Text :
 
     def unique_word (self):
         unique_words = []
-        for word in self.text :
+        for word in self.text.split(' ') :
             if self.frequency_word(word) == 1:
                 unique_words.append(word)
 
@@ -48,13 +48,15 @@ class Text :
         return content_object
 
 stranger = Text.from_file('the_stranger.txt')
-# print(stranger.frequency_word('love'))
-stranger.common_word()
-# print(stranger.unique_word())
+print(stranger.frequency_word('the'))
+stranger.common_word()                            
+print(stranger.unique_word())
 
 
-# text = Text(sentence)       
-# text.unique_word()
+text = Text(sentence)       
+print(text.unique_word())
+print(text.frequency_word('the'))
+text.common_word()
 
 #Part II
 
