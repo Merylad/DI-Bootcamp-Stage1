05d31a2bda7043b97fb131a2d3fd2c8fcc99fd71
Merylad <meryl.adjedj@gmail.com>
2023-05-21 18:49:23 +0300

    exercises W4D1

---
+++ DI_Bootcamp_April2023/Week4/day1/DailyChallenge/dailychallenge.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO actor (first_name, last_name, date_birth, number_oscars)
+-- VALUES ('Jolie', '1980-04-22')   --> It doesn't worl we miss parameters
+
+SELECT COUNT(*) AS actor_count
+FROM actor
+
+
---
+++ DI_Bootcamp_April2023/Week4/day1/ExerciseXP+/exercise.sql
@@ -0,0 +1,34 @@
+-- CREATE TABLE students (
+--     student_id SERIAL PRIMARY KEY,
+--     last_name VARCHAR(100) NOT NULL,
+--     first_name VARCHAR(100) NOT NULL,
+--     date_birth DATE NOT NULL
+-- )
+
+-- INSERT INTO students (last_name, first_name, date_birth)
+-- VALUES
+--     ('Benichou', 'Marc', '1998-11-02'),
+-- 	('Cohen', 'Yoan', '2010-12-03'),
+-- 	('Benichou', 'Lea', '1987-07-27'),
+-- 	('Dux','Amelia','1996-04-07'),
+-- 	('Grez', 'David','2003-06-14'),
+-- 	('Simpson', 'Omer','1980-10-03'),
+-- 	('Adjedj', 'Meryl', '1988-11-24')
+
+-- SELECT * FROM students
+
+-- SELECT * FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc'
+
+-- SELECT * FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc'
+
+-- SELECT * FROM students WHERE first_name LIKE '%a%'
+
+-- SELECT * FROM students WHERE first_name LIKE '%a'
+
+-- SELECT * FROM students WHERE SUBSTRING(first_name, LENGTH(first_name) - 1, 1) = 'a'
+
+-- SELECT *  FROM students WHERE student_id IN (1,3)
+
+-- SELECT * FROM students WHERE date_birth >= '2000-01-01'
+
+
