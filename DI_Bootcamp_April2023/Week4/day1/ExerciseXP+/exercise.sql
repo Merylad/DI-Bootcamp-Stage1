853f561e11361ac87e64ab5a38fc7e7fb4fd3aba
Merylad <meryl.adjedj@gmail.com>
2023-05-21 19:09:26 +0300

    XP GOLD

--- DI_Bootcamp_April2023/Week4/day1/ExerciseXP+/exercise.sql
+++ DI_Bootcamp_April2023/Week4/day1/ExerciseXP+/exercise.sql
@@ -31,4 +31,12 @@
 
 -- SELECT * FROM students WHERE date_birth >= '2000-01-01'
 
+-- SELECT * FROM students ORDER BY last_name LIMIT 4
+
+-- SELECT * FROM students ORDER BY date_birth DESC LIMIT 1
+
+-- SELECT * FROM students OFFSET 2
+
+
+
 
